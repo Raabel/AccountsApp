3c6d5046fdd22a675ca1f1732b49e74615bc1891
Raabel Kothari <kothariraabel@gmail.com>
2021-11-03 12:06:06 +0500

    Add project files.

---
+++ AccountsApp/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsApp
+{
+    class Account
+    {
+        public int Id;
+        public string Name;
+        public decimal Balance;
+        public Account()
+        {
+            System.Windows.Forms.MessageBox.Show("Object is created");
+        }
+        ~Account()
+        {
+            System.Windows.Forms.MessageBox.Show("Obejct is destroyed");
+        }
+    }
+}
---
+++ AccountsApp/AccountForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountsApp
+{
+    public partial class AccountForm : Form
+    {
+        public AccountForm()
+        {
+            InitializeComponent();
+        }
+        Account a;
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            a = new Account();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            a.Id = int.Parse(txtId.Text);
+            a.Name = txtName.Text;
+            a.Balance = decimal.Parse(txtBalance.Text);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            txtId.Text = a.Id.ToString();
+            txtName.Text = a.Name;
+            txtBalance.Text = a.Balance.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtBalance.Clear();
+        }
+
+        private void btnDestroy_Click(object sender, EventArgs e)
+        {
+            a = null;
+        }
+
+        private void btnGC_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBalance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AccountForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
